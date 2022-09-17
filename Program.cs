0823b4283feaf9a8018dea88c8a10a50847b416f
GCaringi <giannicaringi94@gmail.com>
2022-09-17 14:20:48 +0200

    Finish exercise

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     foreach(int elem in array)
     {
-        Console.Write(elem);
+        Console.Write($"{elem} ");
     }
 }
 
@@ -33,4 +33,24 @@ int totalSumArray(int[] array)
     
     return sum;
 }
+
+
+int[] localArray = new[] { 2, 6, 7, 5, 3, 9 };
+
+Console.WriteLine("Stampo l'array");
+stampaArray(localArray);
+
+Console.WriteLine("\nStampo l'array con gli elementi elevati al quadrato");
+int[] powArray = powElemArray(localArray);
+stampaArray(powArray);
+
+Console.WriteLine("\nVerifico se l'array originale non sia stato modificato");
+stampaArray(localArray);
+
+Console.WriteLine("\nStampo la somma di tutti i numeri dell'array originale");
+Console.WriteLine(totalSumArray(localArray));
+
+Console.WriteLine("Stampo la somma di tutti i numeri dell'array con elementi al quadrato");
+Console.WriteLine(totalSumArray(powArray));
+
 
