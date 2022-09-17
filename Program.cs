ff604528e406bdc324b22857e6a336a3b0c249cd
GCaringi <giannicaringi94@gmail.com>
2022-09-17 14:13:39 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿void stampaArray(int[] array)
+{
+    foreach(int elem in array)
+    {
+        Console.Write(elem);
+    }
+}
+
+int pow (int num)
+{
+    return num * num;
+}
+
+int[] powElemArray(int[] array)
+{
+    int[] tmpArray = new int[array.Length];
+
+    for (int i = 0; i < tmpArray.Length; i++)
+    {
+        tmpArray[i] = pow(array[i]);
+    }
+
+    return tmpArray;
+}
+
+int totalSumArray(int[] array)
+{
+    int sum = 0;
+    foreach(int elem in array)
+    {
+        sum += elem;
+    }
+    
+    return sum;
+}
+
